e2b60884859168af51f79a9c0f8907d9a628241d
Michael Dye <mdye@us.ibm.com>
2017-04-11 00:43:47 -0600

    added deduplication and supporting ring buffer implementation

---
+++ buffer/buffer.go
@@ -0,0 +1,104 @@
+package buffer
+
+import (
+	"sync"
+	"time"
+
+	"github.com/michaeldye/synchrophasor-proto/synchrophasor_dpe"
+)
+
+// PublishedDatumEnvelope ...
+type PublishedDatumEnvelope struct {
+	Wrapped     *synchrophasor_dpe.HorizonDatumWrapper
+	PublishedTS int64
+}
+
+// RingBuffer ...
+type RingBuffer struct {
+	data  []*PublishedDatumEnvelope
+	End   int
+	Mutex *sync.Mutex // this is very not awesome, but ok for now
+}
+
+// NewRingBuffer ... size is expected to be small
+func NewRingBuffer(size int) *RingBuffer {
+	return &RingBuffer{
+		data:  make([]*PublishedDatumEnvelope, size),
+		End:   -1,
+		Mutex: &sync.Mutex{},
+	}
+}
+
+// Store ... There is no guarantee that publishFn will be called before writing to the ring, just that a call to this method and successful publishing (or no publishing) entails writing to the ring
+func (r *RingBuffer) Store(wrapped *synchrophasor_dpe.HorizonDatumWrapper, publishFn *func(saved *synchrophasor_dpe.HorizonDatumWrapper) error) error {
+
+	r.Mutex.Lock()
+	defer r.Mutex.Unlock()
+
+	var pubTime int64
+
+	if publishFn != nil {
+		if err := (*publishFn)(wrapped); err != nil {
+			return err
+		}
+
+		pubTime = time.Now().Unix()
+	} else {
+		// not published
+		pubTime = -1
+	}
+
+	if r.End == cap(r.data)-1 {
+		r.End = 0
+	} else {
+		r.End++
+	}
+
+	r.data[r.End] = &PublishedDatumEnvelope{
+		Wrapped:     wrapped,
+		PublishedTS: pubTime,
+	}
+
+	return nil
+}
+
+// PartnerPublished ... TODO: really only sufficient for picking out a partner in a 2-device HA pair, fix that
+func (r *RingBuffer) PartnerPublished(wrapped *synchrophasor_dpe.HorizonDatumWrapper) (int64, string) {
+	// nested iteration, the lists are expected to be *very short*
+
+	r.Mutex.Lock()
+	defer r.Mutex.Unlock()
+
+	// r.End is the most recently written record in the ring
+	ix := r.End
+	for {
+		if ix < 0 {
+			// wrap
+			ix = len(r.data) - 1
+		}
+
+		d := r.data[ix]
+		if d == nil {
+			// these are not sparse
+			break
+		}
+
+		if d.Wrapped.Datum.Id == wrapped.Datum.Id && d.Wrapped.DeviceID != wrapped.DeviceID {
+			for _, p := range d.Wrapped.HAPartners {
+				if p == wrapped.DeviceID {
+					// this message was published by an HA partner
+					return d.PublishedTS, p
+				}
+			}
+		}
+
+		if ix == r.End {
+			// the common base case
+			break
+		}
+
+		ix--
+	}
+
+	return -1, ""
+}
--- main.go
+++ main.go
@@ -14,6 +14,8 @@ import (
 	"google.golang.org/grpc"
 
 	"github.com/michaeldye/synchrophasor-dpe/api"
+	"github.com/michaeldye/synchrophasor-dpe/buffer"
+
 	"github.com/michaeldye/synchrophasor-proto/synchrophasor_dpe"
 )
 
@@ -22,7 +24,9 @@ const (
 	defaultBindRPC  = "0.0.0.0:9009"
 	defaultBindHTTP = "0.0.0.0:8080"
 
-	clientChannelBufferMax = 2e4
+	clientChannelBufferMax = 2e6
+
+	deduplicationBufferMax = 50
 )
 
 // dpeServerImpl is an implementation of the protobuf's interface for a SynchrophasorDPEServer, an interface for storing Synchrophasor data from an edge publisher.
@@ -105,14 +109,42 @@ func (s *dpeServerImpl) Store(stream synchrophasor_dpe.SynchrophasorDPE_StoreSer
 
 func dedup(in <-chan *synchrophasor_dpe.HorizonDatumWrapper) <-chan *synchrophasor_dpe.HorizonDatumWrapper {
 	out := make(chan *synchrophasor_dpe.HorizonDatumWrapper)
+	ring := buffer.NewRingBuffer(deduplicationBufferMax)
+
+	channelPubFn := func(msg *synchrophasor_dpe.HorizonDatumWrapper) error {
+		glog.V(4).Infof("Sending message with id: %v", msg.Datum.Id)
+		out <- msg
+
+		return nil
+	}
 
 	go func() {
 		// continually process input data, deduplicate and send single stream to output
 		for {
+			var pubFn *func(msg *synchrophasor_dpe.HorizonDatumWrapper) error
+
 			select {
 			case msg := <-in:
-				glog.V(6).Infof("Sending message: %v", msg)
-				out <- msg
+
+				glog.V(5).Infof("Considering sending record from partner %v w/ id: %v", msg.DeviceID, msg.Datum.Id)
+
+				if publishedTS, partnerID := ring.PartnerPublished(msg); partnerID != "" {
+
+					if time.Now().Unix()-publishedTS > 5 {
+						glog.V(5).Infof("Publishing duplicate record b/c we haven't ever or recently published from this partner. Msg: %v", msg)
+						pubFn = &channelPubFn
+					} else {
+						glog.V(5).Infof("Dropping duplicate record b/c we last published from partner %v at %v. Msg: %v", partnerID, publishedTS, msg)
+					}
+				} else {
+					glog.V(6).Infof("Publishing record b/c we can't find a recent record of having published the same message by a partner")
+					pubFn = &channelPubFn
+				}
+
+				if err := ring.Store(msg, pubFn); err != nil {
+					glog.Errorf("Failed to publish. Err: %v", err)
+				}
+
 			}
 			runtime.Gosched()
 		}
